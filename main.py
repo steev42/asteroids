ea3e1a8d368281c8519fccbf91abdffb196cae79
steev42 <steev@2d12.com>
2024-09-12 13:22:32 -0400

    chapter 2 part 1

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+
+def main() -> int:
+    print  ("Starting asteroids!")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
