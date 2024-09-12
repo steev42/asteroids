07bac5fa3932c5c465fd7097d1f888b1e40fe482
steev42 <steev@2d12.com>
2024-09-12 14:06:50 -0400

    2.3 complete; game loop and delta time

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main() -> int:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while (True):
         screen.fill(0)
         pygame.display.flip()
@@ -16,6 +19,9 @@ def main() -> int:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+    
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
