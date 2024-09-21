60d453241901ec4023a1273f55b4eda0b654c121
steev42 <steev@2d12.com>
2024-09-21 00:03:13 -0400

    3.4 limiting shot rate

--- player.py
+++ player.py
@@ -4,6 +4,8 @@ from constants import *
 from shot import Shot
 
 class Player(CircleShape):
+    shot_timer = 0
+    
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
@@ -29,6 +31,7 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
     
     def update(self, dt):
+        self.shot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -43,6 +46,9 @@ class Player(CircleShape):
             self.shoot()
     
     def shoot(self):
+        if self.shot_timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation)
         shot.velocity *= PLAYER_SHOOT_SPEED
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
